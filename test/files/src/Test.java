72033402116a78cd5e56fe96092758de09648eb7
Thibaut Durand <thibaut.durand.tdd@gmail.com>
2017-04-11 19:01:53 +0200

    Add some tests for JavaClassFile

---
+++ test/files/src/Test.java
@@ -0,0 +1,76 @@
+
+
+public class Test
+{
+	// Field types
+	
+	private boolean booleanField = false;
+	private byte byteField = (byte)0xf0;
+	private char charField = 'a';
+	private double doubleField = 3.1415926535;
+	private float floatField = 3.1415f;
+	private int intField = 0xf00df00d;
+	private long longField = 0xf00df00ddeadbeefL;
+	private short shortField = (short)0xf00d;
+	
+	private String stringField = "$£€";
+	private int[][][] intTripleArrayField;
+	private String[] stringArrayField = new String[] {"Hello, ", "world"};
+	
+	// Field flags
+	private final int finalField = 123;
+	private static int staticField = 456;
+	private volatile int volatileField = 789;
+	private transient int transientField = 987;
+	public int publicField = 1;
+	int packageField = 2;
+	protected int protectedField = 3;
+	private int privateField = 4;
+	
+	public enum Enum {EnumElement};
+	private Enum enumField = Enum.EnumElement;
+	
+	// Methods parameters and type
+	public void setIntMethod(int n) {}
+	public void setStringMethod(String str) {}
+	public void setArrayMethod(String[] array) {}
+
+	public int getIntMethod() {return 0;}
+	public String getStringMethod() {return "hello";}
+	public String[] getArrayMethod() {return new String[] {};}
+	
+	public int doSomething(String[] array, int index, float value) {return index;}
+	
+	// Methods flags
+	public void publicMethod() {}
+	void packageMethod() {}
+	protected void protectedMethod() {}
+	private void privateMethod() {}
+	public static void staticMethod() {}
+	public final void finalMethod() {}
+	public synchronized void synchronizedMethod() {}
+	public void varargsMethod(Object... obj) {}
+	public native void nativeMethod();	
+	
+	
+	// Interfaces
+	interface Interface
+	{
+		int interfaceField = 0;
+		void interfaceMethod();
+	}
+
+
+	// Classes
+	abstract class AbstractClass
+	{
+		public abstract void abstractMethod();
+	}
+
+	final class FinalClass extends AbstractClass implements Interface
+	{
+		public void interfaceMethod() {}
+		public void abstractMethod() {}
+	}
+}
+
--- test/test-java-class-file.cpp
+++ test/test-java-class-file.cpp
@@ -1,9 +1,148 @@
 #include "test.hpp"
 
+#include <algorithm>
+#include <initializer_list>
 #include "../src/java-class-file.hpp"
+
+
+template<typename T>
+void assertAllIn(const std::vector<T>& elements, const std::vector<T>& all)
+{
+    for(const T& element: elements)
+    {
+        bool isIn = std::any_of(all.begin(), all.end(), [element](T e){return e == element;});
+        ASSERT_TRUE_MSG(isIn, element.str());
+    }
+}
 
 
 void testJavaClassFile()
 {
+    //TODO Test field synthetic
+    //TODO Test method synthetic bridge strict
+    //TODO Test class synthetic annotation
 
+    Class cl;
+    std::vector<Field> fields;
+    std::vector<Method> methods;
+
+    // Test
+    cl = JavaClassFile("test/files/Test.class").javaClass();
+    ASSERT_EQUAL(cl.name, "Test");
+    ASSERT_EQUAL(cl.base, "java.lang.Object");
+    ASSERT_EQUAL(cl.flags, Class::AccPublic | Class::AccSuper);
+
+    fields =
+    {
+        {Type::tBoolean(), "booleanField", Field::AccPrivate},
+        {Type::tByte(), "byteField", Field::AccPrivate},
+        {Type::tChar(), "charField", Field::AccPrivate},
+        {Type::tDouble(), "doubleField", Field::AccPrivate},
+        {Type::tFloat(), "floatField", Field::AccPrivate},
+        {Type::tInt(), "intField", Field::AccPrivate},
+        {Type::tLong(), "longField", Field::AccPrivate},
+        {Type::tShort(), "shortField", Field::AccPrivate},
+        {Type::tObject("java.lang.String"), "stringField", Field::AccPrivate},
+        {Type::tInt(3), "intTripleArrayField", Field::AccPrivate},
+        {Type::tObject("java.lang.String", 1), "stringArrayField", Field::AccPrivate},
+        {Type::tInt(), "finalField", Field::AccPrivate | Field::AccFinal},
+        {Type::tInt(), "staticField", Field::AccPrivate | Field::AccStatic},
+        {Type::tInt(), "volatileField", Field::AccPrivate | Field::AccVolatile},
+        {Type::tInt(), "transientField", Field::AccPrivate | Field::AccTransient},
+        {Type::tInt(), "publicField", Field::AccPublic},
+        {Type::tInt(), "packageField", 0},
+        {Type::tInt(), "protectedField", Field::AccProtected},
+        {Type::tInt(), "privateField", Field::AccPrivate},
+        {Type::tObject("Test$Enum"), "enumField", Field::AccPrivate},
+    };
+
+    assertAllIn(fields, cl.fields);
+
+    methods =
+    {
+        {Type::tVoid(), "publicMethod", {}, Method::AccPublic},
+        {Type::tVoid(), "packageMethod", {}, 0},
+        {Type::tVoid(), "protectedMethod", {}, Method::AccProtected},
+        {Type::tVoid(), "privateMethod", {}, Method::AccPrivate},
+        {Type::tVoid(), "staticMethod", {}, Method::AccPublic | Method::AccStatic},
+        {Type::tVoid(), "finalMethod", {}, Method::AccPublic | Method::AccFinal},
+        {Type::tVoid(), "synchronizedMethod", {}, Method::AccPublic | Method::AccSynchronized},
+        {Type::tVoid(), "varargsMethod", {{{Type::Object, "java.lang.Object", 1}, "param0"}}, Method::AccPublic | Method::AccVarArgs},
+        {Type::tVoid(), "nativeMethod", {}, Method::AccPublic | Method::AccNative},
+        {Type::tVoid(), "setIntMethod", {{Type::tInt(), "param0"}}, Method::AccPublic},
+        {Type::tVoid(), "setStringMethod", {{Type::tObject("java.lang.String", 0), "param0"}}, Method::AccPublic},
+        {Type::tVoid(), "setArrayMethod", {{Type::tObject("java.lang.String", 1), "param0"}}, Method::AccPublic},
+        {Type::tInt(), "getIntMethod", {}, Method::AccPublic},
+        {Type::tObject("java.lang.String"), "getStringMethod", {}, Method::AccPublic},
+        {Type::tObject("java.lang.String", 1), "getArrayMethod", {}, Method::AccPublic},
+        {Type::tInt(), "doSomething", {{Type::tObject("java.lang.String", 1), "param0"}, {Type::tInt(), "param1"}, {Type::tFloat(), "param2"}}, Method::AccPublic},
+    };
+
+    assertAllIn(methods, cl.methods);
+
+
+    // AbstractClass
+    cl = JavaClassFile("test/files/Test$AbstractClass.class").javaClass();
+    ASSERT_EQUAL(cl.name, "Test$AbstractClass");
+    ASSERT_EQUAL(cl.base, "java.lang.Object");
+    ASSERT_EQUAL(cl.flags, Class::AccAbstract | Class::AccSuper);
+
+    methods =
+    {
+        {Type::tVoid(), "abstractMethod", {}, Method::AccPublic | Method::AccAbstract},
+    };
+
+    assertAllIn(methods, cl.methods);
+
+
+    // Enum
+    cl = JavaClassFile("test/files/Test$Enum.class").javaClass();
+    ASSERT_EQUAL(cl.name, "Test$Enum");
+    ASSERT_EQUAL(cl.base, "java.lang.Enum");
+    ASSERT_EQUAL(cl.flags, Class::AccPublic | Class::AccFinal | Class::AccSuper | Class::AccEnum);
+
+    fields =
+    {
+        {Type::tObject("Test$Enum"), "EnumElement", Field::AccPublic | Field::AccStatic | Field::AccFinal | Field::AccEnum},
+    };
+
+    assertAllIn(fields, cl.fields);
+
+
+    // Interface
+    cl = JavaClassFile("test/files/Test$Interface.class").javaClass();
+    ASSERT_EQUAL(cl.name, "Test$Interface");
+    ASSERT_EQUAL(cl.base, "java.lang.Object");
+    ASSERT_EQUAL(cl.flags, Class::AccInterface | Class::AccAbstract);
+
+    fields =
+    {
+        {Type::tInt(), "interfaceField", Field::AccPublic | Field::AccStatic | Field::AccFinal},
+    };
+
+    assertAllIn(fields, cl.fields);
+
+
+    methods =
+    {
+        {Type::tVoid(), "interfaceMethod", {}, Method::AccPublic | Method::AccAbstract},
+    };
+
+    assertAllIn(methods, cl.methods);
+
+
+    // Interface
+    cl = JavaClassFile("test/files/Test$FinalClass.class").javaClass();
+    ASSERT_EQUAL(cl.name, "Test$FinalClass");
+    ASSERT_EQUAL(cl.base, "Test$AbstractClass");
+    ASSERT_EQUAL(cl.flags, Class::AccPublic | Class::AccFinal);
+    ASSERT_EQUAL(cl.interfaces[0], "Test$Interface");
+
+    methods =
+    {
+        {Type::tVoid(), "interfaceMethod", {}, Method::AccPublic},
+        {Type::tVoid(), "abstractMethod", {}, Method::AccPublic},
+    };
+
+    assertAllIn(methods, cl.methods);
 }
