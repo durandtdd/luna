9b4350ffb980c5f6ebecdf091c967c3d1fde8d96
Thibaut Durand <thibaut.durand.tdd@gmail.com>
2017-04-25 18:12:24 +0200

    Rewrote test java file

--- test/files/src/TestCode.java
+++ test/files/src/TestCode.java
@@ -1,4 +1,4 @@
-public class TestCode implements Cloneable
+public class TestCode implements Runnable
 {
 	private int field = 2;
 	private static int staticField = 5;
@@ -17,49 +17,106 @@ public class TestCode implements Cloneable
 		return 1.618;
 	}
 	
-	public int doSomething()
+	public double sum(int p1, double p2)
 	{
-		int var = -6;
-		
-		for(int k=0; k<field; k++)
-		{
-			field += var;
-			var += k;
-		}
-		
-		return field;
+		int a = 3;
+		return p1 + p2 + a;
 	}
 	
-	public int doSomethingElse(int value)
+	public void localVariables()
 	{
+		int a = 1;
+		long b = 2;
+		byte c = 3;
+		float d = 5;
+		double e = a + b - c * d;
+	}
+	
+	public void loop()
+	{
+		for(int k=0; k<10; k++)
+		{
+			int a = 2*k;
+			System.out.println(a);
+		}
+		
+		int k = 1;
+		while(k < 20)
+		{
+			k = 2*k+1;
+		}
+	}
+	
+	public void conditions()
+	{
+		int a = 2;
+		if(5*(a + 1) < 3)
+			a++;
+		else
+			a--;
+		
+		switch(a)
+		{
+			case 0: a += 0; break;
+			case 1: a += 1; break;
+			case 2: a += 2; break;
+			default: a -= 1; break;
+		}
+		
+		switch(a)
+		{
+			case 1: a += 1; break;
+			case 8: a += 8; break;
+			case 16: a += 16; break;
+			default: a -= 1; break;
+		}
+	}
+	
+	public void arrays()
+	{
+		int[] intarr = new int[] {0, 2, 4};
+		double[][] doublearr = new double[][] {{0, 1, 2}, {5, 6, 7}};
+		Object[] objarr = new Object[]{4};
+		
+		int a = intarr[2];
+		a += 2;
+		intarr[0] = a;
+	}
+	
+	public void callMethods()
+	{
+		Object obj = new Object();
+		System.out.println(obj);
+		
+		empty();
+		
 		double d = getDouble();
+		int i = getInt() + (int)d;
+		byte b = (byte)i;
+		int j = b;
 		
-		if(value > 0)
-			d += doSomethingElse(value-1);
-		
-		d -= staticField;
+		double s = sum(getInt(), getDouble());
 		
 		staticMethod();
 		
-		return (int)d;
+		Runnable r = new TestCode();
+		r.run();
 	}
 	
+	@Override
+	public void run()
+	{
+		System.out.println("In a call!");
+	}
+	
+	public void fields()
+	{
+		field += 3;
+		staticField *= 2;
+	}
+
 	public static void staticMethod()
 	{
 		System.out.println("In a static method!");
-	}
-	
-	public Object clone()
-	{
-		Object obj = null;
-		try
-		{
-			super.clone();
-		}
-		catch(java.lang.CloneNotSupportedException e)
-		{
-			System.out.println("Error");
-		}
-		return obj;
 	}
 }
